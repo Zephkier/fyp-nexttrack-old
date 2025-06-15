71333d8019b17c16bebbf65c84754b464df66930
Bn <bnjmn.kmc+github@gmail.com>
2025-06-16 04:40:23 +0800

    feat: add middleware for Spotify API token

--- helper/spotifyApiToken.js
+++ helper/spotifyApiToken.js
@@ -1,19 +1,47 @@
 require("dotenv").config();
 const SpotifyWebApi = require("spotify-web-api-node");
 
+/**
+ * Create new instance of SpotifyWebApi via variables from `.env`.
+ */
 const spotifyApi = new SpotifyWebApi({
     clientId: process.env.SPOTIFY_CLIENT_ID, // Key MUST be named "clientId" and not things like "clientID"
     clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
 });
 
+/**
+ * Helper function to do as its named.
+ *
+ * The Spotify API token gets assigned to `spotifyApi`.
+ */
 async function retrieveSpotifyApiToken() {
     try {
         let data = await spotifyApi.clientCredentialsGrant();
         spotifyApi.setAccessToken(data.body.access_token);
     } catch (err) {
-        console.error(`[!]\nIn ./helper/spotifyApiToken.js > spotifyApi:\n${err}\n[!]`);
+        console.error(`[!]\nIn ./helper/spotifyApiToken.js > retrieveSpotifyApiToken():\n${err}\n[!]`);
         throw err; // Send `err` to Express to inform user
     }
 }
 
-module.exports = { spotifyApi, retrieveSpotifyApiToken };
+/**
+ * Middleware function (note the `request, response, next`) to do as its named.
+ *
+ * Calls `retrieveSpotifyApiToken()` where the Spotify API token gets assigned to `spotifyApi`.
+ */
+async function refreshSpotifyApiToken(request, response, next) {
+    try {
+        await retrieveSpotifyApiToken();
+        next();
+    } catch (err) {
+        console.error(`[!]\nIn ./helper/spotifyApiToken.js > refreshSpotifyApiToken():\n${err}\n[!]`);
+        return response.redirect("/?error=unableToRefreshSpotifyApiToken");
+    }
+}
+
+module.exports = {
+    // Format
+    spotifyApi,
+    retrieveSpotifyApiToken,
+    refreshSpotifyApiToken,
+};
--- routers/index.js
+++ routers/index.js
@@ -2,23 +2,16 @@ const express = require("express");
 const router = express.Router();
 
 const axios = require("axios");
-const { spotifyApi, retrieveSpotifyApiToken } = require("../helper/spotifyApiToken.js");
+const {
+    // Format
+    spotifyApi,
+    retrieveSpotifyApiToken,
+    refreshSpotifyApiToken,
+} = require("../helper/spotifyApiToken.js");
 
-router.get("/", async (request, response) => {
+router.get("/", refreshSpotifyApiToken, (request, response) => {
     // Always set `pageName`
     response.locals.headTitle.pageName = "Home";
-
-    // Always get/refresh Spotify API token
-    try {
-        await retrieveSpotifyApiToken();
-    } catch (error) {
-        /**
-         * TODO
-         * Send to home or error page
-         */
-        console.error(`[!]\nIn ./routers/index.js > .get("/"):\n${error}\n[!]`);
-        return response.redirect("/?error=unableToRetrieveSpotifyApiToken");
-    }
 
     return response.render("./index.ejs", {
         pageName: response.locals.headTitle.pageName,
@@ -45,111 +38,9 @@ router.post("/recommendations", (request, response) => {
     }
 });
 
-// router.get(`/recommendations/:spotify_trackID`, async (request, response) => {
-//     // Always set `pageName`
-//     response.locals.headTitle.pageName = "Recommendations";
-
-//     // Always get/refresh Spotify API token
-//     try {
-//         await retrieveSpotifyApiToken();
-//     } catch (error) {
-//         /**
-//          * TODO
-//          * Send to home or error page
-//          */
-//         console.error(`[!]\nIn ./routers/index.js > .get("/recommendations/:spotify_trackID"):\n${error}\n[!]`);
-//         return response.redirect("/?error=unableToRetrieveSpotifyApiToken");
-//     }
-
-//     try {
-//         // ----- Spotify API
-//         let spotify_trackID = request.params.spotify_trackID;
-//         let spotify_trackData = await spotifyApi.getTrack(spotify_trackID);
-//         // return response.json(spotify_trackData.body); // Check
-
-//         let trackName = spotify_trackData.body.name;
-//         let artistName = spotify_trackData.body.artists[0].name;
-
-//         // ----- Last.fm API
-//         // JSON: /2.0/?method=track.getInfo&api_key=YOUR_API_KEY&artist=cher&track=believe&format=json
-//         /**
-//          * FIXME
-//          * Track/Artist names with special characters (e.g. Florence + The Machine, AC/DC, W&W) does not work 100% of the time.
-//          *
-//          * TEST + (works)
-//          * - Spotify link: Florence + The Machine - Dog Days Are Over @ https://open.spotify.com/track/456WNXWhDwYOSf5SpTuqxd?si=e9a5cc69ef9b4ffe
-//          * - Last.fm site: https://www.last.fm/music/Florence+%252B+the+Machine/_/Dog+Days+Are+Over
-//          * - `endpoint`:   http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=61aa236a275bc7ac487a704ec9dccf12&artist=Florence%20+%20The%20Machine&track=Dog%20Days%20Are%20Over&format=json
-//          *
-//          * TEST / (works)
-//          * - Spotify link: AC/DC - Thunderstruck @ https://open.spotify.com/track/57bgtoPSgt236HzfBOd8kj?si=1f3b7d2fbc074a5e
-//          * - Last.fm site: https://www.last.fm/music/AC%2FDC/_/Thunderstruck
-//          * - `endpoint`:   http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=61aa236a275bc7ac487a704ec9dccf12&artist=AC/DC&track=Thunderstruck&format=json
-//          *
-//          * TEST & (does not work)
-//          * - Spotify link: Dimitri Vegas & Like Mike - Thank You (Not So Bad) @ https://open.spotify.com/track/09CnYHiZ5jGT1wr1TXJ9Zt?si=68c00376f8e7456a
-//          * - Last.fm site: https://www.last.fm/music/Dimitri+Vegas+&+Like+Mike/_/Thank+You+(Not+So+Bad)
-//          * - `endpoint`:   http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=61aa236a275bc7ac487a704ec9dccf12&artist=Dimitri%20Vegas%20&%20Like%20Mike&track=Thank%20You%20(Not%20So%20Bad)&format=json
-//          *
-//          * I've tried:
-//          * - `{artistName}`
-//          * - `{encodeURI(artistName)}`
-//          * - `{encodeURIComponent(artistName)}`
-//          */
-//         let baseUrl = "http://ws.audioscrobbler.com/2.0/";
-//         let method = "track.getInfo";
-//         let apiKey = process.env.LASTFM_API_KEY;
-//         let endpoint = `${baseUrl}?method=${method}&api_key=${apiKey}&artist=${encodeURI(artistName)}&track=${encodeURI(trackName)}&format=json`;
-//         // console.log(`[!]\n${endpoint}\n[!]`);
-
-//         let lastFm_axiosResponse = await axios.get(endpoint);
-//         // // All possible properties from Axios response object
-//         // return response.json({
-//         //     // Check
-//         //     data: lastFm_axiosResponse.data,
-//         //     status: lastFm_axiosResponse.status,
-//         //     statusText: lastFm_axiosResponse.statusText,
-//         //     headers: lastFm_axiosResponse.headers,
-//         //     config: lastFm_axiosResponse.config,
-//         // });
-
-//         /**
-//          * FIXME
-//          * Handle cases when Spotify track is unavailable on Last.fm (e.g. "Playboi Carti" - Bando)
-//          */
-//         // Main focus is `.data` (fyi, the Axios response object must be assigned individually, cannot do it all at once)
-//         let lastFm_genreTags = lastFm_axiosResponse.data.track.toptags.tag;
-
-//         return response.render("./recommendations.ejs", {
-//             pageName: response.locals.headTitle.pageName,
-//             spotify_trackDetails: spotify_trackData.body,
-//             lastFm_genreTags: lastFm_genreTags,
-//         });
-//     } catch (error) {
-//         /**
-//          * TODO
-//          * Send to home or error page
-//          */
-//         console.error(`[!]\nIn ./routers/index.js > .get("/recommendations/:spotify_trackID"):\n${error}\n[!]`);
-//         return response.redirect("/?error=unableToRetrieveTrackData");
-//     }
-// });
-
-router.get(`/recommendations/:spotify_trackID`, async (request, response) => {
+router.get(`/recommendations/:spotify_trackID`, refreshSpotifyApiToken, async (request, response) => {
     // Always set `pageName`
     response.locals.headTitle.pageName = "Recommendations";
-
-    // Always get/refresh Spotify API token
-    try {
-        await retrieveSpotifyApiToken();
-    } catch (error) {
-        /**
-         * TODO
-         * Send to home or error page
-         */
-        console.error(`[!]\nIn ./routers/index.js > .get("/recommendations/:spotify_trackID"):\n${error}\n[!]`);
-        return response.redirect("/?error=unableToRetrieveSpotifyApiToken");
-    }
 
     // Set variables here so they are accessible across try-catch blocks
     let spotify_trackID = request.params.spotify_trackID;
@@ -160,10 +51,8 @@ router.get(`/recommendations/:spotify_trackID`, async (request, response) => {
 
     // Spotify API
     try {
-        spotify_trackID = request.params.spotify_trackID;
-        spotify_trackData = await spotifyApi.getTrack(spotify_trackID);
+        spotify_trackData = await spotifyApi.getTrack(request.params.spotify_trackID);
         // return response.json(spotify_trackData.body); // Check
-
         trackName = spotify_trackData.body.name;
         artistName = spotify_trackData.body.artists[0].name;
     } catch (error) {
@@ -172,7 +61,7 @@ router.get(`/recommendations/:spotify_trackID`, async (request, response) => {
          * Send to home or error page
          */
         console.error(`[!]\nIn ./routers/index.js > .get("/recommendations/:spotify_trackID"):\n${error}\n[!]`);
-        return response.redirect("/?error=unableToRetrieveTrackDataFromSpotify");
+        return response.redirect("/?error=cannotRetrieveTrackDataFromSpotify");
     }
 
     // Last.fm API
@@ -216,7 +105,6 @@ router.get(`/recommendations/:spotify_trackID`, async (request, response) => {
         // But cannot assign all at once, must do it individually
         let lastFm_axiosResponse = await axios.get(endpoint);
         // return response.json(lastFm_axiosResponse.data); // Check
-
         lastFm_genreTags = lastFm_axiosResponse.data.track.toptags.tag;
     } catch (error) {
         /**
@@ -224,7 +112,7 @@ router.get(`/recommendations/:spotify_trackID`, async (request, response) => {
          * Send to home or error page
          */
         console.error(`[!]\nIn ./routers/index.js > .get("/recommendations/:spotify_trackID"):\n${error}\n[!]`);
-        return response.redirect("/?error=unableToRetrieveTrackDataFromLastFm");
+        return response.redirect("/?error=cannotRetrieveTrackDataFromLastFm");
     }
 
     return response.render("./recommendations.ejs", {
