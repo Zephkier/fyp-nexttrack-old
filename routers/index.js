d4a8cb14471af5bdfeb066d879b973768007e753
Bn <bnjmn.kmc+github@gmail.com>
2025-06-16 16:00:24 +0800

    feat: improve home page

--- routers/index.js
+++ routers/index.js
@@ -13,8 +13,48 @@ router.get("/", refreshSpotifyApiToken, (request, response) => {
     // Always set `pageName`
     response.locals.headTitle.pageName = "Home";
 
+    let exampleTracks = [
+        {
+            artistAndName: '"Playboi Carti" - Bando',
+            note: "test an unknown artist",
+            link: "https://open.spotify.com/track/6z7dQwXh9UJJl4wsWxexuI?si=308467749bd94d0d",
+        },
+        {
+            artistAndName: "The Beatles - Something",
+            note: "test a single artist",
+            link: "https://open.spotify.com/track/0pNeVovbiZHkulpGeOx1Gj?si=b9def5c53fe943a7",
+        },
+        {
+            artistAndName: "Selena, benny, Marias - Ojos Tristes",
+            note: "test multiple artists",
+            link: "https://open.spotify.com/track/1DFmBjoeQN9DpOVTEewyx0?si=210d4a8f264e4430",
+        },
+        {
+            artistAndName: "Florence + The Machine - Dog Days Are Over",
+            note: 'test "+" character for Last.fm API',
+            link: "https://open.spotify.com/track/456WNXWhDwYOSf5SpTuqxd?si=e9a5cc69ef9b4ffe",
+            // Last.fm site: https://www.last.fm/music/Florence+%252B+the+Machine/_/Dog+Days+Are+Over
+            // `endpoint`:   http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=61aa236a275bc7ac487a704ec9dccf12&artist=Florence%20+%20The%20Machine&track=Dog%20Days%20Are%20Over&format=json
+        },
+        {
+            artistAndName: "AC/DC - Thunderstruck",
+            note: 'test "/" character for Last.fm API',
+            link: "https://open.spotify.com/track/57bgtoPSgt236HzfBOd8kj?si=1f3b7d2fbc074a5e",
+            // Last.fm site: https://www.last.fm/music/AC%2FDC/_/Thunderstruck
+            // `endpoint`:   http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=61aa236a275bc7ac487a704ec9dccf12&artist=AC/DC&track=Thunderstruck&format=json
+        },
+        {
+            artistAndName: "Dimitri Vegas & Like Mike - Thank You (Not So Bad)",
+            note: 'test "&" character for Last.fm API',
+            link: "https://open.spotify.com/track/09CnYHiZ5jGT1wr1TXJ9Zt?si=68c00376f8e7456a",
+            // Last.fm site: https://www.last.fm/music/Dimitri+Vegas+&+Like+Mike/_/Thank+You+(Not+So+Bad)
+            // `endpoint`:   http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=61aa236a275bc7ac487a704ec9dccf12&artist=Dimitri%20Vegas%20&%20Like%20Mike&track=Thank%20You%20(Not%20So%20Bad)&format=json
+        },
+    ];
+
     return response.render("./index.ejs", {
         pageName: response.locals.headTitle.pageName,
+        exampleTracks: exampleTracks,
     });
 });
 
@@ -51,7 +91,7 @@ router.get(`/recommendations/:spotify_trackID`, refreshSpotifyApiToken, async (r
 
     // Spotify API: Get track name and artist name
     try {
-        spotify_trackData = await spotifyApi.getTrack(request.params.spotify_trackID);
+        spotify_trackData = await spotifyApi.getTrack(spotify_trackID);
         // return response.json(spotify_trackData.body); // Check
         trackName = spotify_trackData.body.name;
         artistName = spotify_trackData.body.artists[0].name;
@@ -70,22 +110,6 @@ router.get(`/recommendations/:spotify_trackID`, refreshSpotifyApiToken, async (r
         /**
          * FIXME
          * Track/Artist names with special characters (e.g. Florence + The Machine, AC/DC, W&W) does not work 100% of the time.
-         *
-         * TEST + (works)
-         * - Spotify link: Florence + The Machine - Dog Days Are Over @ https://open.spotify.com/track/456WNXWhDwYOSf5SpTuqxd?si=e9a5cc69ef9b4ffe
-         * - Last.fm site: https://www.last.fm/music/Florence+%252B+the+Machine/_/Dog+Days+Are+Over
-         * - `endpoint`:   http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=61aa236a275bc7ac487a704ec9dccf12&artist=Florence%20+%20The%20Machine&track=Dog%20Days%20Are%20Over&format=json
-         *
-         * TEST / (works)
-         * - Spotify link: AC/DC - Thunderstruck @ https://open.spotify.com/track/57bgtoPSgt236HzfBOd8kj?si=1f3b7d2fbc074a5e
-         * - Last.fm site: https://www.last.fm/music/AC%2FDC/_/Thunderstruck
-         * - `endpoint`:   http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=61aa236a275bc7ac487a704ec9dccf12&artist=AC/DC&track=Thunderstruck&format=json
-         *
-         * TEST & (does not work)
-         * - Spotify link: Dimitri Vegas & Like Mike - Thank You (Not So Bad) @ https://open.spotify.com/track/09CnYHiZ5jGT1wr1TXJ9Zt?si=68c00376f8e7456a
-         * - Last.fm site: https://www.last.fm/music/Dimitri+Vegas+&+Like+Mike/_/Thank+You+(Not+So+Bad)
-         * - `endpoint`:   http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=61aa236a275bc7ac487a704ec9dccf12&artist=Dimitri%20Vegas%20&%20Like%20Mike&track=Thank%20You%20(Not%20So%20Bad)&format=json
-         *
          * I've tried:
          * - `{artistName}`
          * - `{encodeURI(artistName)}`
